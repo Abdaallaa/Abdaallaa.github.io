dd2acb68bba57422461e02470de635d14969a028
abdaallaa <abdoyaser1998@gmail.com>
2023-02-08 21:54:41 +0200

    add offset for getting customers

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,10 +25,10 @@ const RESOURCES = {
 "icons/Icon-512.png": "c77865282ce6572630d05384e26c64bc",
 "icons/Icon-maskable-192.png": "c77865282ce6572630d05384e26c64bc",
 "icons/Icon-maskable-512.png": "c77865282ce6572630d05384e26c64bc",
-"index.html": "6f4b2effe7b6e2bd2c8f705d2b24db11",
-"/": "6f4b2effe7b6e2bd2c8f705d2b24db11",
+"index.html": "b2e81e1241084cb013860eaa513515c1",
+"/": "b2e81e1241084cb013860eaa513515c1",
 "logo.png": "b0378efe9426ee003f7438abb173ad17",
-"main.dart.js": "febdd2be0ca6007eba0e73b1bc62a3cc",
+"main.dart.js": "a89a0bdcb4735a6d5707dd97460bc405",
 "manifest.json": "1de70ff4cf35addbab07d79fadfab9b8",
 "version.json": "847c655e89f229e86748597c20cf3fb6"
 };
